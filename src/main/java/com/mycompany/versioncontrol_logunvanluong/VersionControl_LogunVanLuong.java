5eb13b006b565c5ed921bab01d4c6c941d4b02d7
Logun <lvanluong@dmacc.edu>
2024-09-28 16:55:21 -0500

    Initial Commit

---
+++ src/main/java/com/mycompany/versioncontrol_logunvanluong/VersionControl_LogunVanLuong.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.versioncontrol_logunvanluong;
+
+/**
+ *
+ * @author Logun
+ */
+public class VersionControl_LogunVanLuong {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
